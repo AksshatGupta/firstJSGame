046709f9c2fee4934d83982912df69fd25d8345e
AksshatGupta <aksshatyashgupta@gmail.com>
2018-12-02 11:56:41 -0500

    Fixing

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const { prompt } = require('enquirer');
+const { prompt } = require('inquirer');
 const { Human, Elf, Dwarf, Goblin, Orc, Pixie, Undead } = require('./scripts.js');
+
+let player;
 
 const promptForClass = async () => {
   const response = await prompt([
@@ -14,35 +16,36 @@ const promptForClass = async () => {
       message: 'What is your username?'
     },
     {
-      type: 'list',
+      type: 'rawlist',
       name: 'race',
       message: 'What race would you prefer to be?',
-      choices: [Human, Elf, Dwarf, Goblin, Orc, Pixie, Undead]
+      choices: ['Human', 'Elf', 'Dwarf', 'Goblin', 'Orc', 'Pixie', 'Undead']
     }
   ]);
   return chooseCharacter(response);
 };
 
 const chooseCharacter = (response) => {
-  if(response.race == Human) {
-    return new Human(response.username);
+
+  if(response.race == 'Human') {
+    player = new Human(response.username);
   }
-  else if(response.race == Elf) {
-    return new Elf(response.username);
+  else if(response.race == 'Elf') {
+    player = new Elf(response.username);
   }
-  else if(response.race == Dwarf) {
-    return new Dwarf(response.username);
+  else if(response.race == 'Dwarf') {
+    player = new Dwarf(response.username);
   }
-  else if(response.race == Orc) {
-   return new Orc(response.username);
+  else if(response.race == 'Orc') {
+   player = new Orc(response.username);
  }
-  else if(response.race == Pixie) {
-   return new Pixie(response.username);
+  else if(response.race == 'Pixie') {
+   player = new Pixie(response.username);
  }
-  else if(response.race == Undead) {
-   return new Undead(response.username);
+  else if(response.race == 'Undead') {
+   player = new Undead(response.username);
  }
 }
 
-const player = promptForClass();
-console.log(player.name);
+promptForClass();
+console.log();
